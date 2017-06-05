8a97597cd1dbafe590a21c4d1151d2d5a2961636
Zane <i.zane.am.robot@gmail.com>
2017-06-05 11:48:53 -0400

    Update example_migration.rb

--- example_migration.rb
+++ example_migration.rb
@@ -2,7 +2,7 @@ class AddRoleToUsers < ActiveRecord::Migration[5.0]
   def change
     add_column :users, :role, :string, default: 'user'
 
-    # Update All Users in Database
+    # Update All Existing Users in Database
     execute "update users set role = 'admin' where is_moderator is true and is_admin is true"
     execute "update users set role = 'moderator' where is_moderator is true and is_admin is false"
     execute "update users set role = 'user' where is_moderator is false and is_admin is false"
